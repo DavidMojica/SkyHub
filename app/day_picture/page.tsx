0130b1363fdef785ccb9489917ebcd5ab7ac09fe
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-05-21 20:20:07 -0500

    Componente PictureOfTheDay
    Cambio de Estilos

--- app/day_picture/page.tsx
+++ app/day_picture/page.tsx
@@ -11,14 +11,16 @@ interface dayPictureInterface {
 export const PictureOfTheDay = (props:dayPictureInterface): React.JSX.Element => {
 
     return (
-        <main className="container font-poppins">
-            <h1>NASA's Astronomy Picture of the Day</h1>
-            <h2>{props.title}</h2>
+        <main className="font-poppins flex justify-center py-8">
+            <article className="container">
+            <h1 className="text-6xl">NASA's Astronomy Picture of the Day</h1>
+            <h2 className="text-xl">{props.title}</h2>
             <p id="date">{props.date}</p>
             <section className="picture-explanation-container">
                 <section id="media_container" dangerouslySetInnerHTML={{ __html: props.media_element }}></section>
               <p>{props.explanation}</p>
             </section>
+            </article>
           </main>
     )
 } 
